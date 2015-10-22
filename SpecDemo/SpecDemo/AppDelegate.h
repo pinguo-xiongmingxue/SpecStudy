42fe59c0c1db56d8f1f7c6e2c0a8f8abc8c35b3e
xmx <xiongmingxue@camera360.com>
2015-10-22 09:45:38 +0800

    我也不知道什么

---
+++ SpecDemo/SpecDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpecDemo
+//
+//  Created by xmx on 15/10/22.
+//  Copyright © 2015年 pinguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
